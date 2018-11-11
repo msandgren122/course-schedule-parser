7e244e3fd46ca5b216b497ed192a14ee35e285de
Matt Sandgren <msandgren122@gmail.com>
2018-11-11 17:03:18 -0500

    some cleaning attempts that are not needed now

--- src/merge.R
+++ src/merge.R
@@ -1,5 +1,16 @@
 install.packages("data.table")
 install.packages("tidyverse")
+install.packages("RCurl")
 
 library(data.table)
 library(tidyverse)
+library(RCurl)
+
+df <- fread("fa10.csv", skip = 13)
+
+df <- read.csv("https://raw.githubusercontent.com/msandgren122/course-schedule-parser/master/csv/fa10.csv",
+               skip = 13)
+
+list.files("https://raw.githubusercontent.com/msandgren122/course-schedule-parser/master/csv")
+
+url <- getURL("https://github.com/msandgren122/course-schedule-parser/tree/master/csv")
