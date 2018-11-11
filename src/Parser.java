e7470971fa3ad68b5f58ec946e63e058a8a48bfb
zachsnoek <zacharysnoek@hope.edu>
2018-11-11 15:19:19 -0500

    Parse commas correctly

--- src/Parser.java
+++ src/Parser.java
@@ -2,51 +2,39 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.ArrayList;
 
 public class Parser {
 	
 	// Parse each csv file and create professor classes, outputting the result to a text file
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		Parser p = new Parser();
+	}
+    public Parser() {
     		HashMap<String, ArrayList<Class>> professors = new HashMap<>(); 
     		String workingDir = "/Users/zacharysnoek/Programming/java/course-schedule-parser/csv/";
         String l = "";
         String cvsSplitBy = ",";
         
-        String fa18 = workingDir + "sp19.csv";
+        String fa18 = workingDir + "fa18.csv";
         
         try (BufferedReader br = new BufferedReader(new FileReader(fa18))) {
 
         		while ((l = br.readLine()) != null) {
-            		
-                String[] line = l.split(cvsSplitBy);
+                ArrayList<String> line = parseLine(l);
+                
                 try {
-                		String comp = line[0].trim();
+                		String comp = line.get(0).trim();
             	   		if (comp.equals("IN PROGRESS") || comp.equals("CANCELLED") || comp.equals("CLOSED") || comp.equals("COMPLETED") || comp.equals("PERMISSION")) {
-            	   			int len = line.length;
-            	   			int titleI = 1;
-            	   			int statusI = 0;
             	   			
-            	   			int firstName = 21;
-            	   			int lastName = 22;
-            	   			int creditI = 6;
-            	   			int capacityI = 17;
-            	   			int actualI = 18;
-            	   			
-            	   			if (len != 25) {
-                	   			firstName++;
-                	   			lastName++;
-                	   			creditI++;
-                	   			capacityI++;
-                	   			actualI++;
-            	   			}
-            	   			String name = line[firstName] + line[lastName];
+            	   			String name = line.get(1);
 
-            	   			String title = line[titleI];
-            	   			String status = line[statusI];
-            	   			String creditAmount = line[creditI];
-            	   			String capacity = line[capacityI];
-            	   			String actual = line[actualI];
+            	   			String title = line.get(1);
+            	   			String status = line.get(0);
+            	   			String creditAmount = line.get(6);
+            	   			String capacity = line.get(17);
+            	   			String actual = line.get(18);
             	   			
             	   			Class c = new Class(title, status, creditAmount, capacity, actual);
             	   			
@@ -62,7 +50,6 @@ public class Parser {
                 		e.printStackTrace();
                 }
             }
-        		
         		for (String name : professors.keySet()) {
         			System.out.println(name);
         			System.out.println(professors.get(name).toString());
@@ -71,5 +58,35 @@ public class Parser {
         } catch (IOException e) {
         		e.printStackTrace();
         }
-	}
+    }
+    
+    public ArrayList<String> parseLine(String line) {
+		char[] input = line.toCharArray();
+		StringBuilder sb = new StringBuilder();
+		ArrayList<String> output = new ArrayList<String>();
+		
+		boolean inQuotes = false;
+		
+		for (char c : input) {
+			if (c == '"') {
+				inQuotes = true;
+			}
+			if (inQuotes == true) {
+				if (c == '"') {
+					inQuotes = false;
+				} else {
+					sb.append(c);
+				}
+			}
+			else if (c == ',') {
+				// Add to array
+				output.add(sb.toString());
+				sb = new StringBuilder();
+			} else {
+				sb.append(c);
+			}
+		}
+		
+		return output;
+    }
 }
